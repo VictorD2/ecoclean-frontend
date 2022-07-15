3cef510f7baa985cbfe58018231006570f425e72
Victor Hernandez <victor-2027@hotmail.com>
2022-07-15 16:14:26 -0500

    chore:

--- config/config.tsx
+++ config/config.tsx
@@ -1,3 +1,3 @@
-// export const API = 'https://ecoclean-backend.herokuapp.com/';
-export const API = 'http://localhost:4000/';
+export const API = 'https://ecoclean-backend.herokuapp.com/';
+// export const API = 'http://localhost:4000/';
 export const URL_PHOTO = 'https://firebasestorage.googleapis.com/b/bucket/o';
